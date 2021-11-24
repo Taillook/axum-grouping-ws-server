d9c51be3b8f95b47896e608c0ddc76b37da51c24
Taillook <jousoku@gmail.com>
2021-11-24 13:22:45 +0900

    Update src/handler/websocket.rs

--- src/handler/websocket.rs
+++ src/handler/websocket.rs
@@ -79,8 +79,8 @@ async fn setup_sender(state: &super::super::AppState, group_id: &str) -> broadca
     let mut group_list = state.group_list.lock().await;
     let group_id = group_id.to_string();
 
-    group_list.entry(group_id).or_insert_with(|| {
-        let (tx, _rx) = broadcast::channel(100);
-        tx
-    }).clone()
+    group_list
+        .entry(group_id)
+        .or_insert_with(|| broadcast::channel(100).0)
+        .clone()
 }
